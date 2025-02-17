59482314f10301cc655fe6627c450a1c8aefdd19
saipranav67 <99899732+saipranav67@users.noreply.github.com>
2025-02-17 15:29:36 +0530

    Update StudentService.java

--- JavaTraining/src/Day6/assignment1/StudentService.java
+++ JavaTraining/src/Day6/assignment1/StudentService.java
@@ -1,5 +1,30 @@
 package Day6.assignment1;
 
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class StudentService {
+    private Set<Student> studentSet;
+
+    public StudentService() {
+        studentSet = new HashSet<>();
+    }
+
+    public boolean addStudent(Student student) {
+        return studentSet.add(student);
+    }
+
+    public void displayStudents() {
+        if (studentSet.isEmpty()) {
+            System.out.println("No students in the set.");
+            return;
+        }
+        for (Student student : studentSet) {
+            System.out.println(student);
+        }
+    }
+
 
 }
+
