ff18aa4c7a66eb5b108026fcbc8051974dfe16f9
saipranav67 <99899732+saipranav67@users.noreply.github.com>
2025-02-17 15:35:37 +0530

    Update Reverse.java

--- JavaTraining/src/Day6/assignment8/Reverse.java
+++ JavaTraining/src/Day6/assignment8/Reverse.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Reverse {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String inputString = scanner.nextLine(); // Read the string
-        char separator = scanner.next().charAt(0); // Read the separator character
+        String inputString = scanner.nextLine(); 
+        char separator = scanner.next().charAt(0); 
         scanner.close();
 
         String result = reshape(inputString, separator);
@@ -14,13 +14,13 @@ public class Reverse {
     }
 
     public static String reshape(String str, char separator) {
-        StringBuilder reversed = new StringBuilder(str).reverse(); // Reverse the string
+        StringBuilder reversed = new StringBuilder(str).reverse(); 
         StringBuilder formatted = new StringBuilder();
 
         for (int i = 0; i < reversed.length(); i++) {
             formatted.append(reversed.charAt(i));
             if (i < reversed.length() - 1) {
-                formatted.append(separator); // Add separator except after the last character
+                formatted.append(separator); 
             }
         }
 
