b2afd70da5c48848ffb29206b55b2ad44b2901ba
saipranav67 <99899732+saipranav67@users.noreply.github.com>
2025-02-17 15:31:51 +0530

    Update EmployeeService.java

--- JavaTraining/src/Day6/assignment4/EmployeeService.java
+++ JavaTraining/src/Day6/assignment4/EmployeeService.java
@@ -8,20 +8,17 @@ public class EmployeeService {
     private Set<Employee> employeeSet;
 
     public EmployeeService() {
-        this.employeeSet = new TreeSet<>(); // TreeSet ensures sorting by empId
+        this.employeeSet = new TreeSet<>(); 
     }
 
-    // 1. Add employees
     public void addEmployee(Employee employee) {
         employeeSet.add(employee);
     }
 
-    // 2. Get all employees
     public List<Employee> getEmployees() {
         return new ArrayList<>(employeeSet);
     }
 
-    // 3. Sort by first name using Comparator
     public List<Employee> sortByFirstName() {
         List<Employee> sortedList = new ArrayList<>(employeeSet);
         sortedList.sort(Comparator.comparing(Employee::getFirstName));
